a5aecf2e0781fa040067ce5a8e990d625bce6c1d
davi.viana <davi.viana@dtidigital.com.br>
2019-03-23 10:45:38 -0300

    web-service

--- app/src/main/java/com/davipviana/retrofitapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/davipviana/retrofitapp/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.davipviana.retrofitapp.ui.activity
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -9,14 +10,18 @@ import com.davipviana.retrofitapp.viewmodel.CepViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var logradouroTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        logradouroTextView = findViewById(R.id.logradouro)
+
         val viewModel = ViewModelProviders.of(this).get(CepViewModel::class.java)
-
-        val textView = findViewById<TextView>(R.id.logradouro)
-
-        textView.text = viewModel.obterLogradouro("30112010")
+        viewModel.obterLogradouro("30112010")
+        viewModel.logradouro.observe(this, Observer {
+            logradouroTextView.text = it
+        })
     }
 }
--- app/src/main/java/com/davipviana/retrofitapp/viewmodel/CepViewModel.kt
+++ app/src/main/java/com/davipviana/retrofitapp/viewmodel/CepViewModel.kt
@@ -1,13 +1,21 @@
 package com.davipviana.retrofitapp.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.davipviana.retrofitapp.repository.CepRepository
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CepViewModel : ViewModel() {
+    val logradouro = MutableLiveData<String>()
+    private val executorService: ExecutorService by lazy {
+        Executors.newSingleThreadExecutor()
+    }
 
-
-    fun obterLogradouro(cep: String): String? {
-        return CepRepository.obterInformacoesCep(cep)?.logradouro
+    fun obterLogradouro(cep: String) {
+        executorService.execute {
+            logradouro.postValue(CepRepository.obterInformacoesCep(cep)?.logradouro)
+        }
     }
 
 }
